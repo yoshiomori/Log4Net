4f149fa2c50e70a6f499fcf695379bf73f44c009
yoshiomori <yoshio.mori@hotmail.com>
2018-08-27 16:28:38 -0300

    Teste AWS elasticsearch com log4net e log4stash

--- Log4Net/Program.cs
+++ Log4Net/Program.cs
@@ -1,16 +1,15 @@
+using log4net;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Log4Net
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            log.Info("ok");
             Console.ReadKey();
         }
     }
